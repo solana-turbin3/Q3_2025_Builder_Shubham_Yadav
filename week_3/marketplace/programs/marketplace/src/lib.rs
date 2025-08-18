07ade11dc8fe77103675633f17167bbf5fe147d3
batmanknows <122247678+batmnnn@users.noreply.github.com>
2025-08-18 19:17:59 +0530

    completed

--- week_3/marketplace/programs/marketplace/src/lib.rs
+++ week_3/marketplace/programs/marketplace/src/lib.rs
@@ -1,16 +1,37 @@
 use anchor_lang::prelude::*;
 
-declare_id!("B4dLvFedN78x3nTZdQHu3uXwUfQkxHWtUuwnC47gt4d2");
+declare_id!("broQPt5f3vtMniWxwJLeHK5X56pGgor4Qmpd9jMVLYT");
+
+pub mod instructions;
+pub mod states;
+pub mod constants;
+pub mod errors;
+
+pub use instructions::*;
 
 #[program]
 pub mod marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_marketplace(ctx: Context<InitializeMarketplace>, fee_percentage: u8) -> Result<()> {
+        ctx.accounts.initialize_marketplace(fee_percentage, ctx.bumps)?;
         Ok(())
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
+    pub fn list_nft(ctx: Context<ListNft>, price: u64) -> Result<()> {
+        ctx.accounts.initialize_listing(price, ctx.bumps)?;
+        ctx.accounts.transfer_nft()
+    }
+
+
+    pub fn delist_nft(ctx: Context<DelistNft>) -> Result<()> {
+        ctx.accounts.transfer_back_nft()
+    }
+
+    pub fn purchase_nft(ctx: Context<PurchaseNft>) -> Result<()> {
+        ctx.accounts.transfer_nft()?;
+        ctx.accounts.transfer_sol()?;
+        ctx.accounts.delist_nft()
+    }
+}
